c042483f0afaad85d463b701723bac2dddfae624
JJ Merelo <jjmerelo@gmail.com>
2023-04-30 11:15:33 +0200

    Ship name in environment variable closes #3
    For some reason, everything is in caps, so...

--- example/example.js
+++ example/example.js
@@ -4,7 +4,7 @@ import axios, {isCancel, AxiosError} from 'axios';
 const socket = new WebSocket("wss://stream.aisstream.io/v0/stream");
 const API_KEY = process.env.AISSTREAM_API_KEY;
 const GEOAPI_KEY = process.env.GEOAPIFY_API_KEY;
-
+const SHIP_NAME = process.env.SHIP_NAME.toUpperCase();
 
 
 socket.addEventListener("open", (_) => {
@@ -28,7 +28,7 @@ socket.addEventListener("message", (event) => {
   let aisMessage = JSON.parse(event.data);
   if (aisMessage["MessageType"] === "PositionReport") {
     const metadata = aisMessage["MetaData"];
-    if (metadata["ShipName"].indexOf("CRYSTAL") >= 0) {
+    if (metadata["ShipName"].indexOf(SHIP_NAME) >= 0) {
       console.warn("Encontrado ", metadata);
       axios.get(`https://api.geoapify.com/v1/geocode/reverse?lat=${metadata["latitude"]}&lon=${metadata["longitude"]}&apiKey=${GEOAPI_KEY}`).then( (response) => {
       const properties = response.data.features[0].properties   
