2b4090e56f616b9a2a9d52d8e818c226261335a4
JJ Merelo <jjmerelo@gmail.com>
2023-04-30 12:13:41 +0200

    Telegram bot started closes #5

--- script/run-bot.js
+++ script/run-bot.js
@@ -1,11 +1,26 @@
 import { Telegraf } from 'telegraf';
+import {readFileSync } from 'fs';
+
+console.log(JSON.parse(readFileSync("./ship-position.json", { encoding: 'utf8', flag: 'r' })))
+let positionData = readData();
+console.log( "Última posición", positionData);
 
 const bot = new Telegraf(process.env.BRBSTR_TOKEN);
+console.log( "Starting bot");
 
-bot.command('barb', (ctx) => ctx.reply('Star'));
-bot.command('star', Telegraf.reply('barb'));
+bot.command('barb', Telegraf.reply('Star'));
+bot.command('star', Telegraf.reply('Barb'));
+bot.command('donde', Telegraf.reply(JSON.stringify(donde())));
 bot.launch();
 
 // Enable graceful stop
 process.once('SIGINT', () => bot.stop('SIGINT'));
 process.once('SIGTERM', () => bot.stop('SIGTERM'));
+
+function donde() {
+    return readData()
+}
+
+function readData() {
+    return JSON.parse(readFileSync("./ship-position.json", { encoding: 'utf8', flag: 'r' }))
+}
